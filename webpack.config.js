9a403b3698d5c2dac6fedd7d6797b00effd55945
Haseeb Zia <haseebzia99@outlook.com>
2020-11-18 16:56:12 +0000

    prod and dev commands using env variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,31 @@
 const path = require('path');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  },
-  module: {
-    rules: [
-      { test: /\.txt$/, use: 'raw-loader' },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
+module.exports = env => {
+  console.log('NODE_ENV: ', env.NODE_ENV);
+  console.log('Production: ', env.production);
+
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+
+    module: {
+      rules: [
+        { test: /\.txt$/, use: 'raw-loader' },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
           }
         }
-      }
-    ]
-  }
-};
+      ]
+    }
+  };
 
+};
